6b9f3bc474b5c56e0bf524c12240c7bb9350065c
Akash7766 <mraakash01932@gmail.com>
2023-02-04 10:38:22 +0600

    package page created

--- src/pages/PackagePage.js
+++ src/pages/PackagePage.js
@@ -10,21 +10,25 @@ const PackagePage = () => {
           our package
         </h1>
       </div>
-      <div className="grid grid-cols-3 gap-10">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10">
         {Package?.data?.map((i) => {
           return (
             <>
-              <div className="my-14 border border-2 p-5 rounded-lg text-center">
-                <div className="text-center mb-5">
-                  <h2 className="text-2xl font-bold uppercase">{i?.name}</h2>
+              <div className="my-14 flex flex-col justify-between border-2 p-5 rounded-lg text-center">
+                <div>
+                  <div className="text-center mb-5">
+                    <h2 className="text-2xl font-bold uppercase">{i?.name}</h2>
+                  </div>
+                  <div className="mb-5">
+                    <h2 className="text-5xl font-bold text-center">
+                      ${i?.price}
+                    </h2>
+                    <h3 className="text-md text-center">Per Month</h3>
+                  </div>
+                  <div className="text-left pl-5">
+                    <div dangerouslySetInnerHTML={{ __html: i?.content }} />
+                  </div>
                 </div>
-                <div className="mb-5">
-                  <h2 className="text-5xl font-bold text-center">
-                    ${i?.price}
-                  </h2>
-                  <h3 className="text-md text-center">Per Month</h3>
-                </div>
-                <div className="text-left">{i?.content}</div>
                 <div className="mt-10">
                   <button className="w-full px-10 py-4 uppercase bg-[#FFF79E] hover:bg-[#EDCF55]">
                     get start today
